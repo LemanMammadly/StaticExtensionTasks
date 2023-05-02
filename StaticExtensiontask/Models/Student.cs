875941e91f5997c8a60f6b115ea11742149697e3
LemanMammad <101627982+LemanMammadly@users.noreply.github.com>
2023-05-02 20:27:23 +0400

    StudentTask

---
+++ StaticExtensiontask/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace StaticExtensiontask.Models;
+
+public class Student
+{
+	public int Id { get; }
+	public string Name { get; set; }
+	public string Surname { get; set; }
+	public static int Count { get; set; }
+
+	public Student()
+	{
+		Count++;
+		Id = Count;
+	}
+}
+
