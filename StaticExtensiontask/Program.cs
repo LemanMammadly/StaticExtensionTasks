cc13b4d4a37a04bb51481289b5ece359b9c1ef08
LemanMammad <101627982+LemanMammadly@users.noreply.github.com>
2023-05-02 21:43:58 +0400

    Task

---
+++ StaticExtensiontask/Program.cs
@@ -0,0 +1,28 @@
+﻿using StaticExtensiontask.Models;
+using StaticExtensiontask.Utilies;
+
+
+#region Task1
+
+Student stu1 = new();
+Student stu2 = new();
+Student stu3 = new();
+Student stu4 = new();
+
+
+Console.WriteLine(stu1.Id);
+Console.WriteLine(stu2.Id);
+Console.WriteLine(stu3.Id);
+Console.WriteLine(stu4.Id);
+
+#endregion
+
+
+
+#region Task2
+
+string str = "Salam Laman. Laman necesen Laman?";
+
+Console.WriteLine(str.MatchCount("Laman")); 
+
+#endregion
