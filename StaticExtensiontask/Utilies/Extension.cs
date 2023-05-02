55824b2478b0a6f8d28da799e693cd8edc5f9ede
LemanMammad <101627982+LemanMammadly@users.noreply.github.com>
2023-05-02 21:35:11 +0400

    Extension

---
+++ StaticExtensiontask/Utilies/Extension.cs
@@ -0,0 +1,27 @@
+﻿namespace StaticExtensiontask.Utilies;
+
+public static class Extension
+{
+	public static int MatchCount(this string str,string search)
+	{
+        int count = 0;
+        for (int i = 0; i <= str.Length - search.Length; i++)
+        {
+            bool match = true;
+            for (int j = 0; j < search.Length; j++)
+            {
+                if (str[i + j] != search[j])
+                {
+                    match = false;
+                }
+            }
+
+            if (match)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
